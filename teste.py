473e317a9bbd4f5682da21dceb3e4f2b38fd40c0
rayanepereira678 <rayane-pereira.rp@academico.ifpb.edu.br>
2026-04-15 12:16:45 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.image("GABI.PNG.jpg")
+st.image("gabi.png")
 st.write("Site Gabrielle Martins")
 st.link_button("Acessar", "https://sites.google.com/academico.ifpb.edu.br/gabriellemartins-info2/in%C3%ADcio")
