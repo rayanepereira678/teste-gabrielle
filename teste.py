d3c2de8e36f741b1f7d2b46d80cd836f520014e2
rayanepereira678 <rayane-pereira.rp@academico.ifpb.edu.br>
2026-04-15 12:07:35 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.image("WhatsApp Image 2026-04-15 at 11.30.34 (1).png")
+st.write("Site Rayane Pereira")
+st.link_button("Acessar", "https://sites.google.com/academico.ifpb.edu.br/rayane/in%C3%ADcio")
