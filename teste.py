3b13419aa536fdb776466ebe9f9b30b5910b9b24
rayanepereira678 <rayane-pereira.rp@academico.ifpb.edu.br>
2026-04-15 12:10:11 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.image("WhatsApp Image 2026-04-15 at 11.30.34 (1).png")
-st.write("Site Rayane Pereira")
-st.link_button("Acessar", "https://sites.google.com/academico.ifpb.edu.br/rayane/in%C3%ADcio")
+st.image("WhatsApp Image 2026-04-15 at 12.00.31")
+st.write("Site Gabrielle Martins")
+st.link_button("Acessar", "https://sites.google.com/academico.ifpb.edu.br/gabriellemartins-info2/in%C3%ADcio")
